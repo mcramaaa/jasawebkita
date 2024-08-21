36c1774d52959fa2d1d88701338adf2f734f05b6
mchramonz <mchrama27@gmail.com>
2024-08-21 17:01:16 +0700

    update

---
+++ ecosystem-dev.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "dev-alpha-landing",
+      script: "yarn",
+      args: "start:prod",
+      cwd: "/var/www/whitelabel/alpha-landing/",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+    },
+  ],
+};
