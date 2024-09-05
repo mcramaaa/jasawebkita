e0f80ba75fd949b8e53dc1607c90cff4b31edace
IvanSahmura <ivansahmura123@gmail.com>
2024-09-05 15:35:52 +0700

    icons-ending-title

--- src/components/landing/Ending.tsx
+++ src/components/landing/Ending.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+  import React from "react";
+  import { WiStars } from "react-icons/wi";
 
-export default function Ending() {
-  return (
-    <div className="mx-4 lg:mx-72 mt-16">
-      <h2 className="text-center flex text-sm lg:mx-40 text-brand-dark font-montserrat flex-col">
-        <span className="text-2xl lg:text-3xl font-bold">
-          Tak kenal maka tak sayang
-        </span>
-        <span>Jangan ragu untuk ngobrol dan konsultasi sekarang.</span>{" "}
-        <span>Jangan sampai kompetitormu yang take action duluan.</span>
-      </h2>
-    </div>
-  );
-}
+  export default function Ending() {
+    return (
+      <div className="mx-4 lg:mx-72 group mt-16">
+        <h2 className="text-center flex text-sm lg:mx-40 text-brand-dark font-montserrat flex-col">
+          <div className="flex flex-row justify-center">
+            <WiStars size={80} className="group-hover:scale-110 duration-300"/>
+              <span className="text-2xl lg:text-3xl font-bold ">
+                Tak kenal maka tak sayang
+              </span>
+            <WiStars size={80} className="group-hover:scale-110 duration-300"/>
+          </div>
+          <span>Jangan ragu untuk ngobrol dan konsultasi sekarang.</span>{" "}
+          <span>Jangan sampai kompetitormu yang take action duluan.</span>
+        </h2>
+      </div>
+    );
+  }
