84e418500813533b1f2b5043a25f399c049df963
IvanSahmura <ivansahmura123@gmail.com>
2024-09-30 11:38:07 +0700

    adding hover to services

--- src/components/landing/Services.tsx
+++ src/components/landing/Services.tsx
@@ -11,7 +11,7 @@ export default function Services() {
       </div>
       <div className='grid grid-cols-1 md:grid-cols-3  gap-6  mx-10  justify-center mt-16'>
         {SERVICESLIST.map ((item, i) =>
-            <div key={i} className='flex items-center text-center py-12 px-12 bg-brand text-brand-bone rounded-3xl'>
+            <div key={i} className='flex items-center text-center py-12 px-12 bg-brand text-brand-bone rounded-3xl hover:scale-110 hover:bg-brand-bone hover:text-brand duration-300'>
                 <div className='flex items-center space-x-4 lg:space-x-6 justify-center'>
                     <span className='animate-bounce text-2xl lg:text-7xl'>{item.icon}</span>
                     <span className='text-lg font-bold'>{item.label}</span>
